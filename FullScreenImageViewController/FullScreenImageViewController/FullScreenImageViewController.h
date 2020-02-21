01fad480e499879ac29a867e4251874508c29ee3
Caleb Rudnicki <calebrudnicki@Calebs-MBP.fios-router.home>
2020-02-21 15:19:01 -0500

    Initial commit

---
+++ FullScreenImageViewController/FullScreenImageViewController/FullScreenImageViewController.h
@@ -0,0 +1,19 @@
+//
+//  FullScreenImageViewController.h
+//  FullScreenImageViewController
+//
+//  Created by Caleb Rudnicki on 2/21/20.
+//  Copyright © 2020 Caleb Rudnicki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FullScreenImageViewController.
+FOUNDATION_EXPORT double FullScreenImageViewControllerVersionNumber;
+
+//! Project version string for FullScreenImageViewController.
+FOUNDATION_EXPORT const unsigned char FullScreenImageViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FullScreenImageViewController/PublicHeader.h>
+
+
---
+++ FullScreenImageViewController/FullScreenImageViewController/FullScreenImageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  FullScreenImageViewController.swift
+//  FullScreenImageViewController
+//
+//  Created by Caleb Rudnicki on 2/21/20.
+//  Copyright © 2020 Caleb Rudnicki. All rights reserved.
+//
+
+import UIKit
+
+class FullScreenImageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+}
